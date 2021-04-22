e41ed33c784f0df4c1f7395b15717c1c8b07ff49
Sunil Vijayan <svijay7487@gmail.com>
2021-04-22 22:37:08 +0400

    Updates

---
+++ precache-manifest.e8b558575c8df3ebfc98c77f8d949400.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e851daa4f4012584779194fe54ee65",
+    "url": "/envy.github.io/index.html"
+  },
+  {
+    "revision": "3ffe9108746a36bea44c",
+    "url": "/envy.github.io/static/css/2.6cfccccb.chunk.css"
+  },
+  {
+    "revision": "c844c3a24b3b08e9584e",
+    "url": "/envy.github.io/static/css/main.37773f12.chunk.css"
+  },
+  {
+    "revision": "3ffe9108746a36bea44c",
+    "url": "/envy.github.io/static/js/2.f6069e52.chunk.js"
+  },
+  {
+    "revision": "90d0fccf45d0ae054589d4c01d82a10a",
+    "url": "/envy.github.io/static/js/2.f6069e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c844c3a24b3b08e9584e",
+    "url": "/envy.github.io/static/js/main.fe1271f3.chunk.js"
+  },
+  {
+    "revision": "1ee967e5bfc49b22510a",
+    "url": "/envy.github.io/static/js/runtime-main.71118dd0.js"
+  },
+  {
+    "revision": "d71a74408d82cdb26e3d3bdc581ba621",
+    "url": "/envy.github.io/static/media/hero-bg-shape-1.d71a7440.svg"
+  },
+  {
+    "revision": "0450221fcde3e98a6e18ddd0ae5bd62c",
+    "url": "/envy.github.io/static/media/home-img.0450221f.png"
+  },
+  {
+    "revision": "4570c307e0a3d29c7b18854540b2a4cf",
+    "url": "/envy.github.io/static/media/img-2.4570c307.png"
+  },
+  {
+    "revision": "1697e46e414dc50b261ca8c10f9884c2",
+    "url": "/envy.github.io/static/media/logo-dark.1697e46e.png"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/envy.github.io/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/envy.github.io/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/envy.github.io/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/envy.github.io/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  }
+]);
